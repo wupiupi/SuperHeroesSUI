03bf2140f25788541d4e8c197aac1b86cb51492a
Paul <pasha.makey@gmail.com>
2024-04-13 13:48:07 +0300

    Initial Commit

---
+++ SuperHeroesSUI/SuperHeroesSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroesSUIApp.swift
+//  SuperHeroesSUI
+//
+//  Created by Paul Makey on 13.04.24.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroesSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
