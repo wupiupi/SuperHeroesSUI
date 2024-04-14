7a1b816b54aa627f69082a0abaf98033a18aa496
Paul <pasha.makey@gmail.com>
2024-04-14 14:25:51 +0300

    🔧 chore: Removed placeholder images

---
+++ SuperHeroesSUI/Components/HeroImageView.swift
@@ -0,0 +1,18 @@
+//
+//  HeroImageView.swift
+//  SuperHeroesSUI
+//
+//  Created by Paul Makey on 14.04.24.
+//
+
+import SwiftUI
+
+struct HeroImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeroImageView()
+}
