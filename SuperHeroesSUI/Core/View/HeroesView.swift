d23919010d33a727110bbb64b10f61c5e5194942
Paul <pasha.makey@gmail.com>
2024-04-15 00:00:44 +0300

    ✨ feature: Search bar

--- SuperHeroesSUI/Core/View/HeroesView.swift
+++ SuperHeroesSUI/Core/View/HeroesView.swift
@@ -9,15 +9,21 @@ import SwiftUI
 
 struct HeroesView: View {
     @EnvironmentObject private var viewVM: ViewModel
+    @State private var searchTerm = ""
     
     private var heroes: [SuperheroInfo] {
         viewVM.heroes
     }
     
+    private var filteredHeroes: [SuperheroInfo] {
+        guard !searchTerm.isEmpty else { return heroes }
+        return heroes.filter { $0.name.localizedCaseInsensitiveContains(searchTerm) }
+    }
+    
     var body: some View {
         NavigationStack {
-            List(heroes) { hero in
-                NavigationLink(destination: HeroDetailsView(hero: hero)){
+            List(filteredHeroes) { hero in
+                NavigationLink(destination: HeroDetailsView(hero: hero)) {
                     HeroCellView(hero: hero)
                 }
                 
@@ -25,6 +31,7 @@ struct HeroesView: View {
             .listStyle(.plain)
             .navigationTitle("Heroes")
         }
+        .searchable(text: $searchTerm, prompt: "Search Heroes")
         .padding()
     }
 }
